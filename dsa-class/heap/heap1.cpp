fd00080f149c52b5434b5688c8d2c5b63cd70d72
Ansuman Pal <ansumanpal16@gmail.com>
2024-11-06 13:49:17 +0530

    heapify and heapsort functions

--- dsa-class/heap/heap1.cpp
+++ dsa-class/heap/heap1.cpp
@@ -58,7 +58,67 @@ int deleteMax()
         }
         if (heap[current])
         {
+            // left to complete
         }
+    }
+}
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = right;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    for(int i = n-1; i >= 0; i--)
+    {
+        //moving current root  to the end;
+        swap(arr[0], arr[i]);
+        //calling max heapify on the redyced heap;
+        heapify(arr, i , 0);
+    }
+}
+
+void printAraay(int arr[], int n){
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int deleteMin()
+{
+    if (heapSize <= 0)
+    {
+        cout << "Heap is empty";
+        cout << endl;
+    }
+    int min = heap[0];
+    heap[0] = heap[heapSize - 1];
+    heapSize--;
+
+    int current = 0;
+    while (current * 2 + 1 < heapSize)
+    {
+        int left = current * 2 + 1;
     }
 }
 
---
+++ dsa-class/heap/heap2.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+        largest = right;
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    for (int i = n / 2; i >= 0; i--)
+    {
+        heapify(arr, n, i);
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        // moving current root  to the end;
+        swap(arr[0], arr[i]);
+        // calling max heapify on the redyced heap;
+        heapify(arr, i, 0);
+    }
+}
+
+void printAraay(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
