022b09152214ce28902368efc7a338997dc1d37d
Ansuman Pal <135434905+Blank-16@users.noreply.github.com>
2025-02-06 20:42:41 +0530

    Reverse integer question leetcode

--- learningForClass/Python.py
+++ learningForClass/Python.py
@@ -22,3 +22,20 @@ def PrimeNumberFinding():
     print(f"Prime numbers between {start} and {end}: {primes}")
 
 
+def FibonacciSeries():
+    def fibonacci(n):
+        if n <= 0:
+            return []
+        elif n == 1:
+            return [0]
+        elif n == 2:
+            return [0, 1]
+        else:
+            fib = [0, 1]
+            for i in range(2, n):
+                fib.append(fib[-1] + fib[-2])
+            return fib
+
+    n = 10
+    fib = fibonacci(n)
+    print(f"Fibonacci series of length {n}: {fib}")
