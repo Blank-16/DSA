ae54b2e749da17b75b84ae22f39197ee412069b2
Blank-16 <ansumanpal16@gmail.com>
2026-05-24 17:51:11 +0530

    code forces few questions

---
+++ codeForces/SlimesOnALime.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+
+            mini = min(mini, x);
+            maxi = max(maxi, x);
+        }
+
+        cout << (maxi - mini + 1) / 2 << endl;
+    }
+    return 0;
+}
